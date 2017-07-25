7836005a05243e05f0dff2e1fbb9f1b0085e1254
DarWafa <wafahssain@gmail.com>
2017-07-25 18:47:08 +0300

    Create,update,delete and some awsome messages

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from .models import Post
 from django.shortcuts import get_object_or_404
+from .forms import PostForm
+from django.contrib import messages
 # Create your views here.
 """def post_create(request):
 	post_list = Post.objects.all()
@@ -31,3 +33,34 @@ def post_list(request):
 	}
 	return render(request,'post_list.html',context)
 
+
+def post_create(request):
+	form = PostForm(request.POST or None)
+	if form.is_valid():
+		form.save()
+		messages.success(request,"OMG! So Cool!")
+		return redirect ("posts:list")
+	context = {
+		"form":form
+	}
+	return render(request,'post_create.html',context)
+
+
+def post_update(request,post_id):
+	post_object = get_object_or_404(Post , id=post_id)
+	form = PostForm(request.POST or None , instance=post_object)
+	if form.is_valid():
+		form.save()
+		messages.success(request,"Giving it a second thought?")
+		return redirect ("posts:list")
+	context = {
+		"form":form,
+		"post_object":post_object,
+	}
+	return render(request,'post_update.html',context)
+
+def post_delete(request, post_id):
+	Post.objects.get(id=post_id).delete()
+	messages.warning(request ,"Seriously bro?")
+	return redirect ("posts:list")
+	
